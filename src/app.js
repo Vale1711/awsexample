2dde0546a8fcd07c5adc1e4fedb91173099d89d5
Vale1711 <152220479+Vale1711@users.noreply.github.com>
2024-01-01 04:58:27 -0500

    cambio

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import languageRoutes from "./routes/language.routes";
 const app = express();
 
 // Settings
-app.set("port", 4000);
+app.set("port", 6157);
 
 // Middlewares
 app.use(morgan("dev"));
